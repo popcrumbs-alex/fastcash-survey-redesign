fe94b1ea20f6e87ebd0ee1c45559e87718121def
Alex <alexroth96@gmail.com>
2021-09-30 12:37:37 -0400

    implement popup modal for t&c/privacy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-console.clear();
+import { privacy, terms } from "./modal_data.js";
+
+const globalState = {
+  isModalOpen: false,
+};
+
 (function () {
   //load year to html for copyright
   const loadDate = () => {
@@ -9,4 +14,67 @@ console.clear();
   };
 
   loadDate();
+})();
+
+(function () {
+  const foundButtons = [...document.querySelectorAll("*[data-type-modal]")];
+  const modal = document.querySelector(".modal--container");
+
+  const processModalContent = (data = []) => {
+    const modalBody = document.querySelector(".modal--container--body");
+    //reset html on each open
+    modalBody.innerHTML = "";
+    return data.forEach((obj) => {
+      //each tet obj should have a headline or text possibly both
+      const headline = document.createElement("h4");
+      const par = document.createElement("p");
+      headline.textContent = obj.headline;
+      par.textContent = obj.text;
+      //paragraph styles
+      par.style.color = "#999";
+      par.style.lineHeight = "1.5";
+      par.style.marginBottom = "1rem";
+      par.style.marginTop = ".5rem";
+      //////////////////////////////
+      headline.style.marginTop = "1rem";
+      headline.style.marginBottom = "0";
+      //Append the elements to the modal container
+      if (obj.headline) modalBody.appendChild(headline);
+      if (obj.text) modalBody.appendChild(par);
+    });
+  };
+
+  const handleModalContent = (data = "") => {
+    const title = document.querySelector("[data-type-modal-title]");
+    console.log("data and title", data, title);
+    switch (data) {
+      case "terms_cond":
+        console.log("Yeet");
+        title.textContent = "Terms & Conditions";
+        processModalContent(terms);
+        break;
+      case "privacy_policy":
+        title.textContent = "Privacy Policy";
+        processModalContent(privacy);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const handleModalState = (data = "") => {
+    globalState.isModalOpen = !globalState.isModalOpen;
+    modal.classList.toggle("hidden");
+    handleModalContent(data);
+  };
+
+  foundButtons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      if (!btn.dataset) {
+        console.error("No modal data provided");
+        return;
+      }
+      handleModalState(btn.dataset.typeModal);
+    });
+  });
 })();
---
+++ src/modal_data.js
@@ -0,0 +1,335 @@
+export const terms = [
+  {
+    headline: "",
+    text: `PLEASE READ THE FOLLOWING TERMS AND CONDITIONS OF USE 
+CAREFULLY BEFORE USING THIS WEBSITE. All users of this site agree that 
+access to and use of this site are subject to the following terms and conditions 
+and other applicable law. If you do not agree to these terms and conditions, 
+please do not use this site.
+`,
+  },
+  {
+    headline: "Copyright",
+    text: `The entire content included in this site, including but not limited to text, graphics 
+or code is copyrighted as a collective work under the United States and other 
+copyright laws, and is the property of fastcashforbusinesses.com. The 
+collective work includes works that are licensed to fastcashforbusinesses.com. 
+Copyright 2003, fastcashforbusinesses.com ALL RIGHTS RESERVED. 
+Permission is granted to electronically copy and print hard copy portions of this 
+site for the sole purpose of placing an order with fastcashforbusinesses.com or 
+purchasing fastcashforbusinesses.com products. You may display and, subject 
+to any expressly stated restrictions or limitations relating to specific material, 
+download or print portions of the material from the different areas of the site 
+solely for your own non-commercial use, or to place an order with 
+fastcashforbusinesses.com or to purchase fastcashforbusinesses.com 
+products. Any other use, including but not limited to the reproduction, 
+distribution, display or transmission of the content of this site is strictly 
+prohibited, unless authorized by fastcashforbusinesses.com. You further agree 
+not to change or delete any proprietary notices from materials downloaded from 
+the site.`,
+  },
+  {
+    headline: "Trademarks",
+    text: `All trademarks, service marks and trade names of fastcashforbusinesses.com 
+used in the site are trademarks or registered trademarks of 
+fastcashforbusinesses.com`,
+  },
+  {
+    headline: "Warranty Disclaimer",
+    text: `This site and the materials and products on this site are provided "as is" and 
+without warranties of any kind, whether express or implied. To the fullest extent 
+permissible pursuant to applicable law, fastcashforbusinesses.com disclaims 
+all warranties, express or implied, including, but not limited to, implied 
+warranties of merchantability and fitness for a particular purpose and noninfringement. fastcashforbusinesses.com does not represent or warrant that the 
+functions contained in the site will be uninterrupted or error-free, that the defects 
+will be corrected, or that this site or the server that makes the site available are 
+free of viruses or other harmful components. fastcashforbusinesses.com does 
+not make any warrantees or representations regarding the use of the materials 
+in this site in terms of their correctness, accuracy, adequacy, usefulness, 
+timeliness, reliability or otherwise. Some states do not permit limitations or 
+exclusions on warranties, so the above limitations may not apply to you.
+`,
+  },
+  {
+    headline: `Limitation of Liability`,
+    text: `fastcashforbusinesses.com shall not be liable for any special or consequential 
+damages that result from the use of, or the inability to use, the materials on this 
+site or the performance of the products, even if fastcashforbusinesses.com has 
+been advised of the possibility of such damages. Applicable law may not allow 
+the limitation of exclusion of liability or incidental or consequential damages, so 
+the above limitation or exclusion may not apply to you.`,
+  },
+  {
+    headline: "Typographical Errors",
+    text: `In the event that a fastcashforbusinesses.com product is mistakenly listed at an 
+incorrect price, fastcashforbusinesses.com reserves the right to refuse or 
+cancel any orders placed for product listed at the incorrect price. 
+fastcashforbusinesses.com reserves the right to refuse or cancel any such 
+orders whether or not the order has been confirmed and your credit card 
+charged. If your credit card has already been charged for the purchase and your 
+order is cancelled, fastcashforbusinesses.com shall issue a credit to your credit 
+card account in the amount of the incorrect price.
+`,
+  },
+  {
+    headline: "Typographical Errors",
+    text: `In the event that a fastcashforbusinesses.com product is mistakenly listed at an 
+incorrect price, fastcashforbusinesses.com reserves the right to refuse or 
+cancel any orders placed for product listed at the incorrect price. 
+fastcashforbusinesses.com reserves the right to refuse or cancel any such 
+orders whether or not the order has been confirmed and your credit card 
+charged. If your credit card has already been charged for the purchase and your 
+order is cancelled, fastcashforbusinesses.com shall issue a credit to your credit 
+card account in the amount of the incorrect price.
+`,
+  },
+  {
+    headline: "Term; Termination",
+    text: `These terms and conditions are applicable to you upon your accessing the site 
+and/or completing the registration or shopping process. These terms and 
+conditions, or any part of them, may be terminated by 
+fastcashforbusinesses.com without notice at any time, for any reason. The 
+provisions relating to Copyrights, Trademark, Disclaimer, Limitation of Liability, 
+Indemnification and Miscellaneous, shall survive any termination.`,
+  },
+  {
+    headline: `Notice`,
+    text: `fastcashforbusinesses.com may deliver notice to you by means of e-mail, a 
+general notice on the site, or by other reliable method to the address you have 
+provided to fastcashforbusinesses.com`,
+  },
+  {
+    headline: "Miscellaneous",
+    text: `Your use of this site shall be governed in all respects by the laws of the state of 
+California, U.S.A., without regard to choice of law provisions, and not by the 
+1980 U.N. Convention on contracts for the international sale of goods. You 
+agree that jurisdiction over and venue in any legal proceeding directly or 
+indirectly arising out of or relating to this site (including but not limited to the 
+purchase of fastcashforbusinesses.com products) shall be in the state or 
+federal courts located in Los Angeles County, California. Any cause of action or 
+claim you may have with respect to the site (including but not limited to the 
+purchase of fastcashforbusinesses.com products) must be commenced within 
+one (1) year after the claim or cause of action arises. 
+fastcashforbusinesses.com's failure to insist upon or enforce strict performance 
+of any provision of these terms and conditions shall not be construed as a 
+waiver of any provision or right. Neither the course of conduct between the 
+parties nor trade practice shall act to modify any of these terms and conditions. 
+fastcashforbusinesses.com may assign its rights and duties under this 
+Agreement to any party at any time without notice to you.
+`,
+  },
+  {
+    headline: "Use of Site",
+    text: `Harassment in any manner or form on the site, including via e-mail, chat, or by 
+use of obscene or abusive language, is strictly forbidden. Impersonation of 
+others, including a fastcashforbusinesses.com or other licensed employee, 
+host, or representative, as well as other members or visitors on the site is 
+prohibited. You may not upload to, distribute, or otherwise publish through the 
+site any content which is libelous, defamatory, obscene, threatening, invasive 
+of privacy or publicity rights, abusive, illegal, or otherwise objectionable which 
+may constitute or encourage a criminal offense, violate the rights of any party 
+or which may otherwise give rise to liability or violate any law. You may not 
+upload commercial content on the site or use the site to solicit others to join or 
+become members of any other commercial online service or other organization.
+`,
+  },
+  {
+    headline: `Participation Disclaimer`,
+    text: `fastcashforbusinesses.com does not and cannot review all communications 
+and materials posted to or created by users accessing the site, and is not in any 
+manner responsible for the content of these communications and materials. 
+You acknowledge that by providing you with the ability to view and distribute 
+user-generated content on the site, fastcashforbusinesses.com is merely acting 
+as a passive conduit for such distribution and is not undertaking any obligation 
+or liability relating to any contents or activities on the site. However, 
+fastcashforbusinesses.com reserves the right to block or remove 
+communications or materials that it determines to be (a) abusive, defamatory, 
+or obscene, (b) fraudulent, deceptive, or misleading, (c) in violation of a 
+copyright, trademark or; other intellectual property right of another or (d) 
+offensive or otherwise unacceptable to fastcashforbusinesses.com in its sole 
+discretion.`,
+  },
+  {
+    headline: `Indemnification`,
+    text: `You agree to indemnify, defend, and hold harmless 
+fastcashforbusinesses.com, its officers, directors, employees, agents, licensors 
+and suppliers (collectively the "Service Providers") from and against all losses, 
+expenses, damages and costs, including reasonable attorneys' fees, resulting 
+from any violation of these terms and conditions or any activity related to your 
+account (including negligent or wrongful conduct) by you or any other person 
+accessing the site using your Internet account.
+`,
+  },
+  {
+    headline: `Third-Party Links
+`,
+    text: `In an attempt to provide increased value to our visitors, 
+fastcashforbusinesses.com may link to sites operated by third parties. 
+However, even if the third party is affiliated with fastcashforbusinesses.com, 
+fastcashforbusinesses.com has no control over these linked sites, all of which 
+have separate privacy and data collection practices, independent of 
+fastcashforbusinesses.com. These linked sites are only for your convenience 
+and therefore you access them at your own risk. Nonetheless, 
+fastcashforbusinesses.com seeks to protect the integrity of its web site and the 
+links placed upon it and therefore requests any feedback on not only its own 
+site, but for sites it links to as well (including if a specific link does not work).`,
+  },
+];
+
+export const privacy = [
+  {
+    headline: "What information do we collect?",
+    text: `We collect information from you when you register on the site, place an order, 
+enter a contest or sweepstakes, respond to a survey or communication such as 
+e-mail, or participate in another site feature.`,
+  },
+  {
+    text: `When ordering or registering, we may ask you for your name, e-mail address, 
+mailing  address,  phone  number,  credit  card  information  or  other  information. 
+You may, however, visit our site anonymously.`,
+  },
+  {
+    text: `We  also  collect  information  about  gift  recipients  so  that  we  can  fulfill  the  gift 
+purchase.  The  information  we  collect  about  gift  recipients  is  not  used  for 
+marketing purposes. `,
+  },
+  {
+    text: `Like many websites, we use "cookies" to enhance your experience and gather 
+information about visitors and visits to our websites. Please refer to the "Do we 
+use 'cookies'?" secti on below for information about cookies and how we use 
+them.`,
+  },
+  {
+    headline: `How do we use your information?`,
+    text: `We may use the information we collect from you when you register, purchase 
+products,  enter  a  contest  or  promotion,  respond  to  a  survey  or  marketing 
+communication,  surf  the  website,  or  use  certain  other  site  features  in  the 
+following ways:`,
+  },
+  {
+    text: `• To personalize your site experience and to allow us to deliver the type of 
+content and    product offerings in which you are most interested.`,
+  },
+  {
+    text: `• To allow us to better service you in responding to your customer service 
+requests.`,
+  },
+  {
+    text: `• To quickly process your transactions.`,
+  },
+  {
+    text: `• To administer a contest, promotion, survey or other site feature.`,
+  },
+  {
+    text: `• If you have opted-in  to  receive  our  e-mail  newsletter,  we  may  send  you 
+periodic e-mails. If you would no longer like to receive promotional e-mail from 
+us, please refer to the "How can you opt-out, remove or modify information you 
+have provided to us?" section below. If you have not opted-in to receive e-mail 
+newsletters,  you  will  not  receive  these  e-mails.  Visitors  who  register  or 
+participate in  other site  features  such as  marketing  programs  and  'members-
+only' content will be given a choice whether they would like to be on our e-mail 
+list and receive e-mail communications from us.`,
+  },
+  {
+    headline: `How do we protect visitor information?`,
+    text: `We  implement  a  variety  of  security  measures  to  maintain  the  safety  of  your 
+personal  information.  Your  personal  information  is  contained  behind  secured 
+networks  and  is  only  accessible  by  a  limited  number  of  persons  who  have 
+special access rights to such systems, and are required to keep the information 
+confidential. When you place orders or access your personal information, we 
+offer the use of a secure server. All sensitive/credit information you supply is 
+transmitted via Secure Socket Layer (SSL) technology and then encrypted into 
+our databases to be only accessed as stated above.`,
+  },
+  {
+    headline: `Do we use "cookies"?`,
+    text: `Yes. Cookies are small files that a site or its service provider transfers to your 
+computer's hard drive through your Web browser (if you allow) that enables the 
+site's or service provider's systems to recognize your browser and capture and 
+remember  certain  information.  For  instance,  we  use  cookies  to  help  us 
+remember and process the items in your shopping cart. They are also used to 
+help us understand your preferences based on previous or current site activity, 
+which enables us to provide you with improved services. We also use cookies 
+to help us compile aggregate data about site traffic and site interaction so that 
+we can offer better site experiences and tools in the future.`,
+  },
+  {
+    text: `We  may  contract  with  third-party  service  providers  to  assist  us  in  better 
+understanding our site visitors. These service providers are not permitted to use 
+the information collected on our be half except to help us conduct and improve 
+our business. `,
+  },
+  {
+    text: `You can choose to have your computer warn you each time a cookie is being 
+sent, or you can choose to turn off all cookies. You do this through your browser 
+(like Netscape Navigator or Internet Explorer) settings. Each browser is a little 
+different, so look at your browser Help menu to learn the correct way to modify 
+your cookies.`,
+  },
+  {
+    text: `If you turn cookies off, you won't have access to many features that make your 
+site  experience  more  efficient  and  some  of  our  services  will  not  function 
+properly. However, you can still place orders over the telephone by contacting 
+customer service.`,
+  },
+  {
+    headline: `Do we disclose the information we collect to outside 
+parties?`,
+    text: `We do not sell, trade, or otherwise transfer to outside parties your personally 
+identifiable information unless we provide you with advance notice, except as 
+described below. The term "outside parties" does not include website hosting 
+partners and o ther parties who assist us in operating our website, conducting 
+our  business,  or  servicing  you,  so  long  as  those  parties  agree  to  keep  this 
+information confidential. We may also release your information when we believe 
+release is appropriate to comply with the law, enforce our site policies, or protect 
+ours or others' rights, property, or safety.`,
+  },
+  {
+    text: `However,  non-personally  identifiable  visitor  information  may  be  provided  to 
+other parties for marketing, advertising, or other uses.`,
+  },
+  {
+    headline: `How can you opt-out, remove or modify information 
+you have provided to us?`,
+    text: `To  modify  your  e-mail  subscriptions,  please  let  us  know  by  modifying  your 
+preferences  in  the  "My  Account"  section.  Please  note  that  due  to  email 
+production schedules you may receive any emails already in production.`,
+  },
+  {
+    text: `To delete all of your online account information from our database, sign into 
+the "My Account" section of our site and remove your shipping addresses, 
+billing address es & payment information. Please note that we may maintain 
+information about an individual sales transaction in order to service that 
+transaction and for record keeping.`,
+  },
+
+  {
+    headline: `Third party links`,
+    text: `In an attempt to provide you with increased value, we may include third party 
+links  on  our  site.  These  linked  sites  have  separate  and  independent  privacy 
+policies.  We  therefore  have  no  responsibility  or  liability  for  the  content  and 
+activities of these linked sites. Nonetheless, we seek to protect the integrity of 
+our  site  and  welcome  any  feedback  about  these  linked  sites  (including  if  a 
+specific link does not work).`,
+  },
+  {
+    headline: `Changes to our policy`,
+    text: `If we decide to change our privacy policy, we will post those changes on this 
+page. Policy changes will apply only to information collected after the date of 
+the change. This policy was last modified on June 11, 2013.`,
+  },
+  {
+    headline: `Questions and feedback`,
+    text: `We welcome your questions, commen ts, and concerns about privacy. Please 
+send us any and all feedback pertaining to privacy, or any other issue.`,
+  },
+  {
+    headline: `Online Policy Only `,
+    text: `This  online  privacy  policy  applies  only  to  information  collected  through  our 
+website and not to information collected offline.`,
+  },
+  {
+    headline: `Your consent`,
+    text: `By using our site, you consent to our privacy policy.`,
+  },
+];
